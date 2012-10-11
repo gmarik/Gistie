1f8c5d71e4a6f0fd243e4827609cbca9f8e626a8
gmarik <gmarik@gmail.com>
2012-10-10 19:54:22 -0500

    Show name/date

--- app.rb
+++ app.rb
@@ -4,14 +4,27 @@ require 'rugged'
 require 'sinatra'
 
 class App < Sinatra::Base
+  configure do
+    enable :inline_templates
+  end
+
   get '/' do
     repo = Rugged::Repository.new(File.dirname(__FILE__))
     commit = repo.lookup(repo.head.target)
-    text = commit.tree.map { |entry| entry[:name] }.join("\n")
-    html = <<-HTML
-    <pre>
-      #{text}
-    </pre>
-    HTML
+    @entries = commit.tree.map do |entry|
+      [entry[:name], Rack::Utils.escape_html(repo.read(entry[:oid]).data)]
+    end
+
+    erb :view
   end
 end
+
+
+__END__
+@@ view
+  <% @entries.each do |n, data| %>
+   <h2><%= n %></h2>
+   <pre>
+   <%= data %>
+   </pre>
+  <% end %>
