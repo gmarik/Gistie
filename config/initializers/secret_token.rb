dd93d58d23b92d2503d4b80866c0f5d05e23b039
gmarik <gmarik@gmail.com>
2012-10-12 01:21:15 -0500

    initial Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitsy::Application.config.secret_token = '2cee6080a66ddc525a1e7a7a4c00d8932b8aada4f200e454aa5ff72df91af41ba5c294786970ad6138ef7a3cd42b0cf4d5111662bf26a377b7204f0e38d6e849'
