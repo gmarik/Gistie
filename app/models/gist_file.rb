f3946ba461d6443b7302ec16d5c28c29200f8bb7
gmarik <gmarik@gmail.com>
2012-10-13 18:44:22 -0500

    minor refactor

--- app/models/gist_file.rb
+++ app/models/gist_file.rb
@@ -1,14 +1,21 @@
 class GistBlob
   include ActiveModel::Validations
 
-  attr_accessor :blob, :name#, :description
+  VALID_ATTRS= [
+    :name,
+    :blob
+  ]
+
+  attr_accessor *VALID_ATTRS
 
   validates :blob, { presence: true }
 
+  # since it's not an ActiveRecord
+  # TODO: does ActiveModel have smth for this
   def self.from_params(params)
-    new.tap do |gf|
-      params.keys.each do |k|
-        gf.send("#{k}=", params[k])
+    new.tap do |gb|
+      params.slice(*VALID_ATTRS).each do |k, v|
+        gb.send("#{k}=", v)
       end
     end
   end
