fcd74ad416566be59263d7792d7d29ce1f4d3967
gmarik <gmarik@gmail.com>
2012-10-14 01:31:28 -0500

    implement GistWriter
    - spec and model

---
+++ app/models/gist_writer.rb
@@ -0,0 +1,71 @@
+class GistWriter
+
+  attr_reader :repo
+
+  def initialize(repo)
+    @repo = repo
+  end
+
+  def write(blobs)
+    author = {
+      email: "gitsy@gitsy.com", 
+      time: Time.now,
+      name: "gitsy"
+    }
+
+    named_blob_oids = write_blobs(blobs)
+    tree_oid = write_tree(named_blob_oids)
+    commit_oid = write_commit(tree_oid, author)
+    set_master(commit_oid)
+    commit_oid
+  end
+
+  def write_blobs(blobs)
+    blobs.map do |b|
+      # oid - "object id" is a SHA1 hash
+      oid = repo.write(b.blob, :blob)
+      [b.name, oid]
+    end
+  end
+
+  def write_tree(blob_named_sha1s, builder = Rugged::Tree::Builder.new )
+    blob_named_sha1s.each do |name, oid|
+      builder << {
+        type: :blob,
+        name: name,
+        oid:  oid,
+        filemode: 33188
+      }
+    end
+
+    tree_sha1 = builder.write(repo)
+  end
+
+  def write_commit(tree_sha1, author, parents = commit_parent, commit = Rugged::Commit)
+    commit_sha1 = commit.create(repo,
+      author:      author,
+      message:     "Commit\n\n",
+      committer:   author,
+      parents:     parents,
+      tree:        tree_sha1
+    )
+  end
+
+  def commit_parent
+    if repo.empty? then []
+    else                [repo.head.target]
+    end
+  end
+
+  def set_master(commit_oid)
+    ref_name =  "refs/heads/master"
+    r = Rugged::Reference
+    if repo.empty?
+      r.create(repo, ref_name, commit_oid)
+    else
+      ref = r.lookup(repo, ref_name)
+      ref.target = commit_oid
+    end
+    commit_oid
+  end
+end
---
+++ spec/models/gist_writer_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe GistWriter do
+
+  def repo_dir; '/tmp/123'; end
+
+  let(:repo)   { Rugged::Repository.init_at(repo_dir, true) }
+  let(:writer) { GistWriter.new(repo) }
+  let(:a_blob) { mock(blob: "Holla!", name: "README.txt") }
+
+  it "writes stuff" do
+    sha1 = writer.write([a_blob])
+    repo.head.target.should == sha1
+  end
+
+  describe '.write_blobs' do
+    it "writes blobs to repo" do
+      repo.should_receive(:write).
+        with("Holla!", :blob).
+        and_return('oid')
+
+      writer.write_blobs([a_blob]).
+        should == [['README.txt', 'oid']]
+    end
+  end
+
+  describe '.write_tree' do
+    it "writes tree repo" do
+      builder = mock(:builder)
+      entry = {
+        type: :blob,
+        name: 'README.txt',
+        oid:  'oid',
+        filemode: 33188
+      }
+
+      builder.should_receive(:<<).
+        with(entry)
+      builder.should_receive(:write).
+        with(repo).
+        and_return('toid')
+
+      named_sha1 = ['README.txt', 'oid']
+      writer.write_tree([named_sha1], builder).
+        should == 'toid'
+    end
+  end
+
+  describe '.write_commit' do
+    it 'writes commit' do
+      commit = mock(:commit)
+      commit.should_receive(:create).
+        with(repo, {
+          author: 'author',
+          message: "Commit\n\n",
+          committer: 'author',
+          parents: ['poid'],
+          tree: 'toid'
+        }).and_return('coid')
+
+      writer.write_commit('toid', 'author', ['poid'], commit).should == 'coid'
+    end
+  end
+
+end
