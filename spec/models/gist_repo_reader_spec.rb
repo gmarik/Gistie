af1ad3ed71d26f68b1b315c39e1b83089509821f
gmarik <gmarik@gmail.com>
2012-10-19 23:10:30 -0500

    GistRepoReader
    - spec and module
    - it's a duplicate of GistReader for now to not break specs

---
+++ spec/models/gist_repo_reader_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe GistRepoReader do
+  subject do
+    Class.new do
+      include GistRepoReader
+      def repo
+        Rugged::Repository.new(fixture_repo_path)
+      end
+    end.new
+  end
+
+  its(:repo_read) do
+    should == [
+      {:name=>"README.md", :oid=>"8aa2aed323077103c91369c23e886c6bae2d987d", :filemode=>33188, :type=>:blob, :content=>"# A README.md\n"}, 
+      {:name=>"file.txt", :oid=>"82d1aa8da25e22d908c44bde9d122fea688ba3a0", :filemode=>33188, :type=>:blob, :content=>"a file.txt\n"}
+    ]
+  end
+end
