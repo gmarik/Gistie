3b25ab07061084484d185f1bf4b4def134f24187
gmarik <gmarik@gmail.com>
2012-11-05 16:43:20 -0600

    gist info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
-  def font_awesome_icon(class_name, text)
+  def font_awesome_icon(class_name, text = '')
     %Q[<i class="icon-#{class_name}"></i> #{text}].html_safe
   end
 
   alias_method :i, :font_awesome_icon
+
+  def public_clone_url(gist)
+    hostname = if defined?(HOSTNAME) then HOSTNAME else request.host end
+    "git@#{hostname}:#{gist.id}.git"
+  end
 end
