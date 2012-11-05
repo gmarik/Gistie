924d3caff3b2a2781fec44e93381fb043b0f9ede
gmarik <gmarik@gmail.com>
2012-11-05 09:12:42 -0600

    more font-awesome icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def font_awesome_icon(class_name, text)
+    %Q[<i class="icon-#{class_name}"></i> #{text}].html_safe
+  end
+
+  alias_method :i, :font_awesome_icon
 end
