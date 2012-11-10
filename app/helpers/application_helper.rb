1df3ced33db67d6b724bb2bb5ccf47ef4ae6f08b
gmarik <gmarik@gmail.com>
2012-11-10 09:24:56 -0600

    public clone url is git://

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,6 @@ module ApplicationHelper
 
   def public_clone_url(gist)
     hostname = if defined?(HOSTNAME) then HOSTNAME else request.host end
-    "git@#{hostname}:#{gist.id}.git"
+    "git://#{hostname}/#{gist.id}.git"
   end
 end
