e7243645dd16e513a7ae02c8348adfa0e57f6b90
gpurkins <george.purkins@gmail.com>
2013-02-26 18:16:45 -0500

    Added inline HTML to blob controller, raw output is better
    Added inlined HTML to blob controller, formatting is better.
    
    Undo routing experiment

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -3,6 +3,7 @@ class BlobsController < ApplicationController
 
   def show
     @gist = Gist.find(params[:gist_id])
-    render text: @gist.repo.lookup(params[:id]).read_raw.data, :layout => false
+    #render text: @gist.repo.lookup(params[:id]).read_raw.data, :layout => false
+    render :inline => "<html><body style=\"white-space:pre-wrap\">" + @gist.repo.lookup(params[:id]).read_raw.data + "</body></html>"
   end
 end
